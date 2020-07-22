d146f8c9439baad9eda5cc1e66f1667cdaf2df5d
Admin <admin@Picos-iMac.local>
2020-07-22 16:35:01 +0700

    adding Simplenav on top

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Simplenav from './components/Simplenav'
 
 function App() {
   return (
     <div className="App">
       <h1>Application started</h1>
+      <Simplenav/>
     </div>
   );
 }
---
+++ src/components/Simplenav.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Simplenav=()=>{
+    return(
+        <div>
+        <div class="ui large top fixed hidden menu">
+          <div class="ui container">
+            <a class="active item">Home</a>
+            <a class="item">Work</a>
+            <a class="item">Company</a>
+            <a class="item">Careers</a>
+            <div class="right menu">
+              <div class="item">
+                <a class="ui button">Log in</a>
+              </div>
+              <div class="item">
+                <a class="ui primary button">Sign Up</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="ui vertical inverted sidebar menu">
+          <a class="active item">Home</a>
+          <a class="item">Work</a>
+          <a class="item">Company</a>
+          <a class="item">Careers</a>
+          <a class="item">Login</a>
+          <a class="item">Signup</a>
+        </div>
+        </div>
+    );
+}
+
+export default Simplenav;
