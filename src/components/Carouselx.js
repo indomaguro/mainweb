5f624dcf6a48d877dc9544db89595f0e787be228
Admin <admin@Picos-iMac.local>
2020-07-23 15:27:23 +0700

    update library images

--- src/components/Carouselx.js
+++ src/components/Carouselx.js
@@ -1,5 +1,7 @@
 import React,{useEffect} from 'react';
 import imgMukadimah from './images/mukadimah.jpg';
+import imgBrands from './images/fishop-brands.png';
+import imgStore from './images/Fishop_store2.jpg';
 //import $ from 'jquery';
 //import ReactDOM from 'react-dom';
 import { 
@@ -20,7 +22,7 @@ const Carouselx=()=>{
         <Carousel.Item>
           <img
             className="d-block w-100 carox"
-            src={imgMukadimah}
+            src={imgStore}
             alt="First slide"
           />
           <Carousel.Caption>
@@ -28,30 +30,31 @@ const Carouselx=()=>{
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item>
           <img
             className="d-block w-100 carox"
-            src={imgMukadimah}
-            alt="Third slide"
+            src={imgBrands}
+            alt="Second slide"
           />
-      
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item>
           <img
             className="d-block w-100 carox"
             src={imgMukadimah}
             alt="Third slide"
           />
-      
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
           </Carousel.Caption>
         </Carousel.Item>
+
       </Carousel>
 
         </div>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import image1 from './images/mukadimah.jpg';
 import Carouselx from './Carouselx';
-import imgDadu from './images/dadu.jpg';
+import imgDadu from './images/unagi.jpg';
 import { 
   Container,
   Row,
@@ -29,20 +29,22 @@ const Homepage=()=>{
     return(
         <div>
      
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" sticky="top" >
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <Nav className="mx-auto">
+            <NavDropdown title="EAT & DRINK" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#link">MEMBERSHIP</Nav.Link>
+            <Nav.Link href="#link">SHOP</Nav.Link>
+            <Nav.Link href="#link">NEWS</Nav.Link>
+            <Nav.Link href="#link">CORPORATE</Nav.Link>
           </Nav>
           <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -56,13 +58,13 @@ const Homepage=()=>{
       <Container className="eat-drink">
         <Row>
           <Col xs={6} md={4}>
-            <Image src={imgDadu} rounded />
+            <Image src={imgDadu} thumbnail />
           </Col>
           <Col xs={6} md={4}>
-            <Image src={imgDadu} rounded />
+            <Image src={imgDadu} thumbnail />
           </Col>
           <Col xs={6} md={4}>
-            <Image src={imgDadu} rounded />
+            <Image src={imgDadu} thumbnail />
           </Col>
         </Row>
       </Container>        
