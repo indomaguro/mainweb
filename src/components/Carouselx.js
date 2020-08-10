f2a1b7d5dd18afd7b2049785f791c7c3cad50239
Admin <admin@Picos-iMac.local>
2020-08-10 17:31:39 +0700

    Remove Carousel reactjs-boostrap component from Home.js

--- src/components/Carouselx.js
+++ src/components/Carouselx.js
@@ -7,9 +7,7 @@ import img3 from './images/8551.jpg';
 //import imgStore from './images/Fishop_store2.jpg';
 //import $ from 'jquery';
 //import ReactDOM from 'react-dom';
-import { 
-    Carousel
-  } from 'react-bootstrap';
+//import {Carousel} from 'react-bootstrap';
 
 const Carouselx=()=>{
 
@@ -19,7 +17,53 @@ const Carouselx=()=>{
 
     
     return(
-        <div>
+    <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
+      <ol className="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      </ol>
+      <div className="carousel-inner">
+        <div className="carousel-item active imagebox">
+          <img src={img1} className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </div>
+        </div>
+        <div className="carousel-item imagebox">
+          <img src={img2} className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+        <div className="carousel-item imagebox">
+          <img src={img3} className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </div>
+        </div>
+      </div>
+      <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    );
+}
+
+export default Carouselx;
+
+
+
+
+        {/*<div>
 
         <Carousel>
         <Carousel.Item>
@@ -67,8 +111,4 @@ const Carouselx=()=>{
 
       </Carousel>
 
-        </div>
-    );
-}
-
-export default Carouselx;
+    </div>*/}
