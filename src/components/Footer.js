07e028ed9bc53e03e3a109c3c4a3d4e0ac18f9f4
Admin <admin@Picos-iMac.local>
2020-08-10 09:17:24 +0700

    update footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,35 +8,33 @@ import {
 const Footer=()=>{
     return(
 
-        <div className="footerpart">
-        <Container>
-          <Row>
-            <Col>
+        <div className="container footerpart">
+          <div className="row">
+            <div className="col">
               <h3>MShop Indonesia</h3>
               <p>
               MShop Indonesia is a high-end grocery store that specialises in selling seafood (fresh or frozen), meat, frozen food and other kinds of ingredients essential for cooking!  
                
               </p>
-            </Col>
-            <Col>
+            </div>
+            <div className="col">
                 <h3>Explore</h3>
                 <ul className="exp-link">
                     <li><a href="/">Home</a></li>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">Home</a></li>
+                    <li><a href="/member">Membership</a></li>
+                    <li><a href="https://fishop.co.id/" target="_blank">Fishop</a></li>
+                    <li><a href="/news">News</a></li>
+                    <li><a href="/about">About</a></li>
                 </ul>
-            </Col>
-            <Col>
+            </div>
+            <div className="col">
                 <h3>Connect With Us !</h3>
                 <i className="fas fa-phone-volume"></i> 021-2926-3576 or 0811-919-6688<br/>
                 <i className="fab fa-whatsapp"></i> 0815-1913-1635<br/>
-                <i className="fab fa-facebook"></i> fishop_jkt<br/>
-                <i className="fab fa-instagram"> fishop.jkt</i>
-            </Col>
-          </Row>
-        </Container>      
+                <i className="fab fa-facebook"></i> ForEat<br/>
+                <i className="fab fa-instagram"> foreat_id</i>
+            </div>
+          </div>   
         </div>
   
 
